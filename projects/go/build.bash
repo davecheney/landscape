cb8cdf101f17a0b3993718e2a3112061232d737d
Dave Cheney <dfc@deadwood.fruckers.org>
2011-05-01 11:31:35 +0400

    Refactor

--- projects/go/build.bash
+++ projects/go/build.bash
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+TAG=$1
 source ./env
-source $1/env
 
-(cd $WORKSPACE/src && ./make.bash)
+TAG_DIR=$PROJECT_ROOT/tags/$TAG
+source $TAG_DIR/env
+
+function build-number() {
+	hg identify $WORKSPACE -n
+}
+
+export -f build-number
+BUILD_DIR=$TAG_DIR/builds/$(build-number)
+mkdir -p $BUILD_DIR
+
+exec >$BUILD_DIR/stdout.err \
+bash -c '
+	cd $WORKSPACE/src 
+	exec ./make.bash
+'
 
